f81740185e91d71e3bb0667e48799dd2f352b8ce
Robert Weller <rob31989@yahoo.com>
2018-05-29 10:53:36 -0400

    Module 13 Completed

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -11,22 +11,22 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute
-              ,private router: Router) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id');
+    const id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
     this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    }
+      'productId': 1,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
   }
   onBack(): void {
     this.router.navigate(['/products']);
