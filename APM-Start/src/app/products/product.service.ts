37da538652b0ad3d14717bbb9210d4b0d26d0100
Robert Weller <rob31989@yahoo.com>
2018-05-29 14:44:00 -0400

    Updated local angular cli to 6.0.5

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,20 +1,17 @@
+import { tap, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-
+import { Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class ProductService {
     private productUrl = './api/products/products.json';
     constructor (private http: HttpClient) {}
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productUrl)
-            .do(data => JSON.stringify(data))
-            .catch(this.handleError);
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+                 tap(data => JSON.stringify(data)),
+                 catchError(this.handleError));
     }
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
